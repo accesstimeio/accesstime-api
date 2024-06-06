6dda25f5477adcd2d611757155fe69a0c598c350
mehmetraufoguz <88249309+mehmetraufoguz@users.noreply.github.com>
2024-06-06 13:35:03 +0300

    chore: add config module
    add joi package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import Joi from "joi";
+
+const NODE_ENV = process.env.NODE_ENV;
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: !NODE_ENV ? ".env" : `.env.${NODE_ENV}`,
+            isGlobal: true,
+            validationSchema: Joi.object({
+                SUBGRAPH_URL: Joi.string().required()
+            })
+        })
+    ],
     controllers: [],
     providers: []
 })
