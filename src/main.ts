a8720be73d1ac3455119332a87f2426dcee886ab
mehmetraufoguz <88249309+mehmetraufoguz@users.noreply.github.com>
2024-06-12 20:16:09 +0300

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,25 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    const allowlist = [
+        "http://localhost:5173",
+        "https://accesstime.io",
+        "https://app.accesstime.io"
+    ];
+    const corsOptionsDelegate = function (req: any, callback: any) {
+        let corsOptions: { origin: boolean };
+        if (allowlist.indexOf(req) !== -1) {
+            corsOptions = { origin: true };
+        } else {
+            corsOptions = { origin: false };
+        }
+        callback(null, corsOptions);
+    };
+
+    app.enableCors({
+        origin: corsOptionsDelegate
+    });
+
     const config = new DocumentBuilder()
         .setTitle("AccessTime")
         .setDescription("Dashboard API")
