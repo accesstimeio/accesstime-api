ad0a12c2c347a03e0a45fdce579bf524cceeda63
mehmetraufoguz <88249309+mehmetraufoguz@users.noreply.github.com>
2025-03-14 12:27:51 +0300

    chore(deployment): update listDeployments
    add totalCount to dto

--- src/modules/deployment/deployment.service.ts
+++ src/modules/deployment/deployment.service.ts
@@ -86,6 +86,7 @@ export class DeploymentService {
                         page: 0,
                         maxPage: 0,
                         deployments: [],
+                        totalCount: 0,
                         pageCursor: null
                     };
                 }
@@ -108,6 +109,7 @@ export class DeploymentService {
                 page: requestedPage,
                 maxPage: Math.floor(deploymentCount / limit),
                 deployments: listDeployments.deployments,
+                totalCount: deploymentCount,
                 pageCursor: listDeployments.pageCursor
             };
 
